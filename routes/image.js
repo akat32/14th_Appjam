8b9a61492bdc3ae9564e120bad459c852dd27493
akat32 <baduk32127@gmail.com>
2017-12-17 04:34:09 +0900

    asdsadsad

--- routes/image.js
+++ routes/image.js
@@ -1,6 +1,6 @@
 module.exports = (router, Users, fs, path)=>{
   router.get('/image/:image', function(req, res, next) {
-    fs.readFile('/root/meouk/14th_Appjam/image/iOPunZW5rgor.PNG', (error,data)=>{
+    fs.readFile('./image/iOPunZW5rgor.PNG', (error,data)=>{
         console.log(data);
         res.end(data);
     });
