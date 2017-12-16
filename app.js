ad7888fbf5db89c97539efc00aa8ea56d24bcf5a
akat32 <baduk32127@gmail.com>
2017-12-17 03:31:40 +0900

    add upload

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var path = require('path');
 var fs = require('fs');
 require('./mongo');
 var app = express();
+var upload = require('./routes/upload');
 var auth = require('./routes/auth')(express.Router(), Users, rndstring, School);
 var school = require('./routes/school')(express.Router(),School, rndstring);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({limit: '1gb', extended: false }));
 app.use('/auth',auth);
 app.use('/school', school);
+app.use('/upload',upload);
 app.listen(3000, ()=>{
   console.log('Server Porting on 3000');
 })
